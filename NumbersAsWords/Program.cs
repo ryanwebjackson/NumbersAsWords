d788da280abcf182339c5573c36782f76ade4fc6
Ryan Jackson <ryanjackson0y-@live.com>
2019-10-22 21:21:28 -0500

    Add CSharp example of Numsense
    Covers basic integer conversion. Consider contributing to Numsense to address additional use cases:  https://github.com/ploeh/Numsense

---
+++ NumbersAsWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Ploeh.Numsense.ObjectOriented;
+
+namespace NumbersAsWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "one-thousand, two-hundred, sixty two"; //best-case scenario
+            //var input = "zero one two three four five six seven eight nine ten";
+            var cleaned = input.Replace(",", string.Empty);
+            var eachWord = cleaned.Split(' ');
+            List<int> resultingDigits = new List<int>();
+            int counter = 0;
+            foreach (var word in eachWord)
+            {
+                var success = Numeral.English.TryParse(word, out int result);
+                if (success)
+                {
+                    resultingDigits.Add(result);
+                    counter += result;
+                }
+            }
+            Console.WriteLine("Result: " + counter);
+        }
+    }
+}
